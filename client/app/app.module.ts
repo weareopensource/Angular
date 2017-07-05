ad74748b5eb69a82368e2294b707733abcc85627
Mohamed FARHAT <mohamed.farhat.09@gmail.com>
2017-07-05 13:43:01 +0200

    comment debug

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -50,7 +50,7 @@ import { UsersModule } from 'app/users';
 export class AppModule {
   // Diagnostic only: inspect router configuration
   constructor(router: Router) {
-    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+    // console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
   }
 
 }
