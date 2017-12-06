e3160e89dbd73961e14875f44974759ae4a103a1
Foad Ardalan <foad.ardalan@outlook.fr>
2017-12-06 18:45:59 +0100

    fixing login bug

--- src/app/authentication/services/authentication.interceptor.ts
+++ src/app/authentication/services/authentication.interceptor.ts
@@ -13,31 +13,42 @@ import { Router } from '@angular/router';
 import * as AuthenticationActions from '../store/authentication.actions';
 import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/throw';
-import { AuthenticationState } from '../store';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/withLatestFrom';
+import { AuthenticationState, AuthenticationSelectors } from '../store';
+import { isEmpty } from 'lodash';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
   private urlFilters = ['/api'];
+  private tokenExpiresIn$;
 
-  constructor(private store: Store<AuthenticationState>) {}
+  constructor(private store: Store<AuthenticationState>, private authenticationSelectors: AuthenticationSelectors) {
+    this.tokenExpiresIn$ = this.store.select(this.authenticationSelectors.getTokenExpiresIn)
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
-    if (request.url.match(/\assets\//)) {
+    if (request.url.match(/(?!\api\/)/)) {
       return next.handle(request);
     }
-    if (parseInt(sessionStorage.getItem('tokenExpiresIn'), 10) < Date.now()) {
-      this.store.dispatch(new AuthenticationActions.Logout('Token Expired'));
-      return Observable.empty();
-    }
-    return next
-    .handle(request)
-    .catch(error => {
-      if (error instanceof HttpErrorResponse && error.status === 403 && !error.url.includes('/auth')) {
-        this.store.dispatch(new AuthenticationActions.Logout('Unauthorized Operation'));
+
+    return Observable.of()
+    .withLatestFrom(this.tokenExpiresIn$)
+    .switchMap(tokenExpiresIn => {
+      if (tokenExpiresIn && tokenExpiresIn < Date.now()) {
+        this.store.dispatch(new AuthenticationActions.Logout('Token Expired'));
         return Observable.empty();
       }
-      return Observable.throw(error);              
+      return next
+      .handle(request)
+      .catch(error => {
+        if (error instanceof HttpErrorResponse && error.status === 403 && !error.url.includes('/auth')) {
+          this.store.dispatch(new AuthenticationActions.Logout('Unauthorized Operation'));
+          return Observable.empty();
+        }
+        return Observable.throw(error);              
+      });
     });
   }
 }
