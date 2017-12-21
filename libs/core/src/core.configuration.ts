326aba8dd6f4349cb888dc44447fd7b45fe5701e
Foad Ardalan <foad.ardalan@outlook.fr>
2017-12-21 11:34:08 +0100

    removing the 404 menu link

--- libs/core/src/core.configuration.ts
+++ libs/core/src/core.configuration.ts
@@ -11,12 +11,6 @@ export const coreConfiguration: CoreConfiguration = {
       link: '/home',
       name: 'Home',
       icon: 'action:ic_home_24px'
-    },
-    {
-      order: 2,
-      link: '/test',
-      name: 'Test',
-      icon: 'action:ic_pan_tool_24px'
     }
   ]
 };
