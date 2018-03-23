a6d5daa1f2b43381ece794b309e3b7486f107eec
Foad Ardalan <foad.ardalan@outlook.fr>
2018-03-23 10:51:49 +0100

    complete the crud

---
+++ libs/task/src/components/detail/task-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { fromRouter } from '@labdat/router-state';
+import { Store } from '@ngrx/store';
+import { selectCurrentTask } from '@labdat/task-state';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+
+@Component({
+  selector: 'app-task-detail',
+  templateUrl: './task-detail.component.html',
+  styleUrls: ['./task-detail.component.scss']
+})
+export class TaskDetailComponent implements OnInit {
+  public comments$: Observable<Array<any>>;
+
+  constructor(private store: Store<any>) {}
+
+  ngOnInit(): void {
+    this.comments$ = this.store
+    .select(selectCurrentTask)
+    .pipe(map(task => task.comments));
+  }
+
+  close(): void {
+    this.store.dispatch(new fromRouter.Back());
+  }
+}
