629473d10efebcb60204ff1b1d69ca8f40f1d8bf
Foad Ardalan <foad.ardalan@outlook.fr>
2018-04-30 10:49:46 +0200

    fix selectSelectedUser selector

--- libs/user/src/components/detail/user-detail.page.component.ts
+++ libs/user/src/components/detail/user-detail.page.component.ts
@@ -1,26 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { selectSelectedUser } from '../../+state/user.selectors';
 import { UserState } from '../../+state/user.interfaces';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   templateUrl: './user-detail.page.component.html',
-  styleUrls: ['./user-detail.page.component.scss']
+  styleUrls: ['./user-detail.page.component.scss'],
 })
-export class UserDetailPageComponent implements OnInit, OnDestroy {
-  public user: any;
-  private subscriptions: Subscription;
+export class UserDetailPageComponent {
+  public currentUser$ = this._store.select(selectSelectedUser);
+
   constructor(private _store: Store<UserState>) { }
-
-  ngOnInit(): void {
-    this.subscriptions = this._store.select(selectSelectedUser)
-    .subscribe((user: any) => {
-      this.user = user;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
-  }
 }
