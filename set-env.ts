8b9122a276ba77b16d256198fe70943a9448902e
Foad Ardalan <foad.ardalan@outlook.fr>
2018-06-15 13:52:23 +0200

    redefining environments

--- set-env.ts
+++ set-env.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs';
 import * as _ from 'lodash';
 import * as objectPath from 'object-path';
-import { environment as fileEnv } from './apps/default/src/environments/environment.prod';
+import { environment as fileProdEnv } from './apps/default/src/environments/environment.prod';
+import { environment as fileDevEnv } from './apps/default/src/environments/environment';
+
+const fileEnv = (process.env.NODE_ENV === 'prod') ? fileProdEnv : fileDevEnv;
 
 let sysEnv = _.pickBy(process.env, (_value, key) => key.startsWith('WAOS_FRONT_'));
 
@@ -12,12 +15,14 @@ const sysMemEnv = {};
 _.forEach(sysEnv, (v, k) => objectPath.set(sysMemEnv, k, v));
 const env = _.merge(fileEnv, sysMemEnv);
 
-fs.open('./apps/default/src/environments/environment.prod.ts', 'w', (err, fd) => {
+const filePath = `./apps/default/src/environments/${(process.env.NODE_ENV === 'prod') ? 'environment.prod.ts' : 'environment.ts'}`;
+
+fs.open(filePath, 'w', (err, fd) => {
   if (err) {
     throw err;
   }
   const envConfigFile = `export const environment = ${
-    JSON.stringify(env, null, 2)
+    JSON.stringify(env, undefined, 2)
     .replace(/\"([^(\")"]+)\":/g, '$1:')
     .replace(/"/g, '\'')};`;
   fs.writeSync(fd, envConfigFile);
